82eeb9c88c600845136a22e5e1161f00170c060e
Sebastian Soto M <s.m.sebastian.n@gmail.com>
2024-11-17 14:17:53 -0600

    styles

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {},
+    tailwindcss: {}, // Load TailwindCSS
+    autoprefixer: {}, // Recommended for vendor prefixing
   },
 };
 
