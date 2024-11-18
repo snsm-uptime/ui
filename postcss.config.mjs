9d3eddbabd89dab92fdc891f7970400fea697ee4
Sebastian Soto M <s.m.sebastian.n@gmail.com>
2024-11-17 18:18:49 -0600

    cleanup

--- postcss.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {}, // Load TailwindCSS
-    autoprefixer: {}, // Recommended for vendor prefixing
-  },
-};
-
-export default config;
--- src/models/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./ApiResponse";
-export * from "./Transaction";
-export * from "./Pagination";
