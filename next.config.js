d5e7b2310814721f773c76704988aea7707f8ea4
Sebastian Soto M <s.m.sebastian.n@gmail.com>
2024-11-19 17:21:20 -0600

    working post and get

--- next.config.js
+++ next.config.js
@@ -2,8 +2,12 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: "/api/:path*",
-        destination: `${process.env.API_BASE_URL}/v1/:path*/`,
+        source: "/api/transactions",
+        destination: `${process.env.API_BASE_URL}/v1/transactions/`,
+      },
+      {
+        source: "/api/transactions/pull",
+        destination: `${process.env.API_BASE_URL}/v1/transactions/pull`,
       },
     ];
   },
